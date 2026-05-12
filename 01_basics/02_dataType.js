0619da27037d5624530c956fee15e219188c0ff9
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2026-05-12 13:54:58 +0545

    datatype conversion

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs,  not browser
+
+console.log(3 + 3)
+console.log("Govinda")
+
+
+let name = "Govinda"
+let age = 21
+let isLoggedIn = false
+let status = null
+
+// primitive datatype
+// number => 2 to power of 53
+//  bigint => not used usually
+// "string" => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not defined. variable declared but not assigned
+// symbol => uniqueness 
+
+//object:{}
+
+console.log(typeof name)
+
+console.log(typeof null) // object
+
+console.log(typeof undefined) // undefined
+
+
--- test.js
+++
@@ -1,3 +0,0 @@
-console.log("Govinda")
-
-console.log("hello world")

294c1d8531189996e2ebda7d4f5da192b5f8868b
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2026-05-12 14:05:55 +0545

    operation in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,26 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ****************** Operations ***************
+ 
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 *2)
+// console.log(2 ** 3) // 2 ko power 3
+// console.log(2/3)
+// console.log(2 % 3) 
+
+let str1 = "govinda"
+let str2 = "chaudhary"
+let str3 = str1 + str2
+console.log(str3) 
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32

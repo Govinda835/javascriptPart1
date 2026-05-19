0bf5375158a600196bc501e3315c1cda27bd2b4f
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2026-05-19 23:06:03 +0545

    scopes in js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,12 +4,46 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner: ",a);
+   // console.log("Inner: ",a);
     
 }
 
- console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "govinda"
+    function two(){
+        const website = "youtube.."
+        console.log(username)
+        // console.log(website);
+    }
+    
+    two()
+}
+one()
 
+if(true){
+    const username = "govinda"
+    if(username === "govinda"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }  
+}
+
+
+// ************** interesting *************
+
+function addOne(num){
+    return num + 1
+}
+console.log(addOne(5));
+
+
+const addTeo = function (num){
+    return num +2
+}
+console.log(addTeo(5))
+//console.log(addTeo);

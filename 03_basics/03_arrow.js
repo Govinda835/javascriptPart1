deb57e37ab8d66c1ae84d72ea8a49eb0719e14d1
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2026-05-21 00:22:27 +0545

    arrow function and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -11,3 +11,31 @@ const user = {
 user.welcomeMessage()
 user.username = "hitesh"
 user.welcomeMessage()
+
+//console.log(this);
+
+// function chai ()
+// {
+//     let username = "govinda"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai  =  () => {
+
+//     let username = "govinda"
+//     console.log(this.username);   
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(2,4))
+
+const addTwo = (num1, num2) => (num1 + num2) // monstly used in react
+console.log(addTwo(2,4))
+
+const returnObject = () => ({username : "govinda"}) // to return object 
+console.log(returnObject())

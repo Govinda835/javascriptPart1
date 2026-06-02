6eb06c895ec21dbd03ce6160cf567016053a7217
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2026-06-02 12:21:50 +0545

    if and nested if

---
+++ 05_advance/if.js
@@ -0,0 +1,39 @@
+// let age = 25;
+// if(age >= 18){
+//     console.log("You are old enough to enter this site")
+// }
+// else{
+//     console.log("You must be 18+ to enter this site...")
+// }
+
+const time = 12
+if(time < 12){
+    console.log("good morning")
+}
+else{
+    console.log("good afternoon");
+    
+}
+
+let isStudent = true
+if(isStudent){
+    console.log("you are a student");
+    
+}
+else{
+    console.log("you are not a student");
+}
+
+let age = 18
+let license = false
+if(age >= 16){
+    console.log("you are old enough to drive");
+     if(license){
+        console.log("you can drive");
+        
+     }
+}
+else{
+    console.log("you are not allow to drive");
+    
+}
